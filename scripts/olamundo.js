230beb9f598dddaa60e100407df354add0a9c2cf
Felipe Tomm <felipe.tomm@gmail.com>
2018-05-07 22:44:18 -0300

    Código Aula-02

---
+++ scripts/olamundo.js
@@ -0,0 +1,4 @@
+var p = document.createElement('p');
+var ola = "DevDojo vocês são f***";
+p.textContent = ola;
+document.body.appendChild(p);
---
+++ scripts/olamundo.ts
@@ -0,0 +1,4 @@
+var p = document.createElement('p');
+var ola: string = "DevDojo vocês são f***";
+p.textContent = ola;
+document.body.appendChild(p);
