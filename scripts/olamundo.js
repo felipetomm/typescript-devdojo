0e4949942cbfa13d071e68eacb95b23151518206
Felipe Tomm <felipe.tomm@gmail.com>
2018-05-07 23:05:09 -0300

    Código Aula-03

--- scripts/olamundo.js
+++ scripts/olamundo.js
@@ -2,3 +2,39 @@ var p = document.createElement('p');
 var ola = "DevDojo vocês são f***";
 p.textContent = ola;
 document.body.appendChild(p);
+var num = 10;
+var bool = true;
+var str = "A beatiful message";
+var str2 = 'A beatiful message with aspas simples';
+var str3 = str2 + " \n                    to Felipe";
+console.log(str3);
+function func() { }
+;
+if (bool != null) { }
+;
+if (bool != undefined) { }
+;
+if (bool) {
+    //var i: number; //Variavel se torna global
+    var i = void 0;
+    for (i = 0; i < 3; i++) {
+        console.log(i);
+    }
+    console.log("Valor do I dentro do escopo: " + i);
+}
+//console.log("Valor do I fora do escopo "+i);
+//Array
+var list = [1, 2, 3];
+var list2 = [1, 2, 3];
+//Tupla
+var tuple;
+tuple = ["Felipe", 24];
+console.log("Tupla: " + tuple[0]);
+//Enumaração
+var Day;
+(function (Day) {
+    Day[Day["Monday"] = 0] = "Monday";
+    Day[Day["Tuesday"] = 1] = "Tuesday";
+})(Day || (Day = {}));
+var dia = Day.Monday;
+console.log("Day: " + Day[dia]);
--- scripts/olamundo.ts
+++ scripts/olamundo.ts
@@ -2,3 +2,40 @@ var p = document.createElement('p');
 var ola: string = "DevDojo vocês são f***";
 p.textContent = ola;
 document.body.appendChild(p);
+
+let num: number = 10;
+let bool: boolean = true;
+let str: string = "A beatiful message";
+let str2: string = 'A beatiful message with aspas simples';
+let str3: string = `${str2} 
+                    to Felipe`;
+console.log(str3);
+
+function func():void{};
+
+if(bool != null){};
+if(bool != undefined){};
+
+if(bool){
+    //var i: number; //Variavel se torna global
+    let i: number;
+    for(i=0;i<3;i++){
+        console.log(i);
+    }
+    console.log("Valor do I dentro do escopo: "+i);
+}
+//console.log("Valor do I fora do escopo "+i);
+
+//Array
+let list: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+//Tupla
+let tuple:[string,number];
+tuple = ["Felipe",24];
+console.log("Tupla: "+tuple[0]);
+
+//Enumaração
+enum Day{Monday,Tuesday}
+let dia: Day = Day.Monday;
+console.log("Day: "+Day[dia]);
